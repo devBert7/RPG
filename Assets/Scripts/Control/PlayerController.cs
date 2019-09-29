399e62255f79e9a85900c667034515c05503329c
Robert Thiel <thielbert7@gmail.com>
2019-09-28 19:28:35 -0600

    Created CombatTarget.cs - attached to the enemy prefab, and Fighter.cs attached to the player... both using correct namespaces.  In PlayerController.cs, extracted MoveToCursor() from Update and named new method MovementInteraction(), still called within Update as well as newly created CombatInteraction() method.  CombatInteraction() uses Physics.RaycastAll and calls GetMouseRay() to grab the mouse position from the main camera's ScreenPointToRay() method.  I've also used this method to replace extraced mouse position from MoveToCursor() method.  An array is returned from RaycastAll of all items the ray encounters.  If the mouse is clicked and one of those items is the CombatTarget component, the public Attack() method within Fighter.cs is called and prints a message to the console.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,23 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.control {
 	public class PlayerController : MonoBehaviour {
 		void Update() {
+			CombatInteraction();
+			MovementInteraction();
+		}
+
+		void CombatInteraction() {
+			RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+			foreach(RaycastHit hit in hits) {
+				CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+				if (target) {
+					if (Input.GetMouseButtonDown(0)) {
+						GetComponent<Fighter>().Attack(target);
+					}
+				}
+			}
+		}
+
+		void MovementInteraction() {
 			if (Input.GetMouseButton(0)) {
 				MoveToCursor();
 			}
 		}
 
 		void MoveToCursor() {
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			bool hasHit = Physics.Raycast(ray, out hit);
+			bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
 			if (hasHit) {
 				GetComponent<Mover>().MoveTo(hit.point);
 			}
+		}
+
+		static Ray GetMouseRay() {
+			return Camera.main.ScreenPointToRay(Input.mousePosition);
 		}
 	}
 }
