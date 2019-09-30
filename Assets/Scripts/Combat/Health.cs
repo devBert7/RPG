38711d7a638cbd6146b0d9781a248cd8b1d06406
Robert Thiel <thielbert7@gmail.com>
2019-09-30 15:07:44 -0600

    Included a isDead bool initialized to false.  When HP reaches 0 (or less), Death animator trigger is set off and changes isDead to true - animating the death once only.  Next - get player to stop attacking after character is dead.

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,9 +6,15 @@ namespace RPG.Combat {
 	public class Health : MonoBehaviour {
 		[SerializeField] float health = 100f;
 
+		bool isDead = false;
+
 		public void TakeDamage(float damage) {
 			health = Mathf.Max(health - damage, 0);
-			print(health);
+
+			if (health == 0 && !isDead) {
+				GetComponent<Animator>().SetTrigger("Die");
+				isDead = true;
+			}
 		}
 	}
 }
