8f21a85346ab6c02b0320d5d3af0082271bd8598
Robert Thiel <thielbert7@gmail.com>
2019-10-01 11:31:47 -0600

    Created AIController.cs and created a distance check between the enemy and the player, and whether it was within a chaseRange variable's distance.  If so, the enemy printed to the console "Should chase now".  This sets up logic for the enemy to chase the player within 5f distance and attack in upcoming commits.

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control {
+	public class AIController : MonoBehaviour {
+		[SerializeField] float chaseDistance = 5f;
+
+		void Update() {
+			GameObject player = GameObject.FindWithTag("Player");
+			bool isInRange = Vector3.Distance(player.transform.position, transform.position) <= chaseDistance;
+
+			if (isInRange) {
+				print(gameObject.name + " Should Chase Now");
+			}
+		}	
+	}
+}
