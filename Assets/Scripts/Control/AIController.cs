4e6e9fd73d3f8139fffe07c73390097e4e4cadc3
Robert Thiel <thielbert7@gmail.com>
2019-10-02 11:02:15 -0600

    Added suspicion behavior so the guard does't automatically return to his post when out of range, instead waits in suspicion for a moment before returning.  Added suspicionTime and timeSinceChase variables which respectively tell the enemy how long to wait before giving up against how much time has passed since last chase.  Within Update(), added else if() statement which cancels the current action, allowing the guard to walk to the last position they saw the player and wait for the susicionTime before returning to post.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,12 +8,14 @@ using RPG.Movement;
 namespace RPG.Control {
 	public class AIController : MonoBehaviour {
 		[SerializeField] float chaseDistance = 5f;
+		[SerializeField] float suspicionTime = 5f;
 
 		Fighter fighter;
 		GameObject player;
 		Health health;
 		Vector3 guardPosition;
 		Mover mover;
+		float timeSinceChase = Mathf.Infinity;
 
 		void Start() {
 			fighter = GetComponent<Fighter>();
@@ -31,10 +33,15 @@ namespace RPG.Control {
 			bool inAttackRange = Vector3.Distance(player.transform.position, transform.position) <= chaseDistance;
 
 			if (inAttackRange && fighter.CanAttack(player)) {
+				timeSinceChase = 0;
 				fighter.Attack(player);
+			} else if (timeSinceChase < suspicionTime) {
+				GetComponent<ActionScheduler>().CancelCurrentAction();
 			} else {
 				mover.StartMovementAction(guardPosition);
 			}
+
+			timeSinceChase += Time.deltaTime;
 		}
 
 		void OnDrawGizmosSelected() {
