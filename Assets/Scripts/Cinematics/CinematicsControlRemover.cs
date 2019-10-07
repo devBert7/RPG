79392fe5f24c9f6c8df7684d7558b5023e3c3418
Robert Thiel <thielbert7@gmail.com>
2019-10-07 10:20:02 -0600

    Working to understand Delegates and Events in C# while implementing the Playable Director Events in Unity.  New CinematicsControlRemover.cs grabs the PlayableDirector and utilized played and stopped events to then call DisableControl() and EnableControl() methods both of which, for now, print to the console 'disable / enable controls', respectively.  Next, let's actually disable and enable the player controller so we avoid our player running into enemies during our cinema scene.

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics {
+	public class CinematicsControlRemover : MonoBehaviour {
+		PlayableDirector director;
+		void Start() {
+			director = GetComponent<PlayableDirector>();
+			director.played += DisableControl;
+			director.stopped += EnableControl;
+		}
+
+		void DisableControl(PlayableDirector disable) {
+			print("Disable Control");
+		}
+
+		void EnableControl(PlayableDirector enable) {
+			print("Enable Control");
+		}
+	}
+}
