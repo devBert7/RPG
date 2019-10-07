42971b697f8ddd38c0169bf303a87095746d8de3
Robert Thiel <thielbert7@gmail.com>
2019-10-06 18:36:11 -0600

    Created new namespace - RPG.Cinematics and new script within > CinematicTrigger.cs where OnCollisionEnter(Collider other) was used to trigger our cinematic moment when the GameObject.FindWithTag("Player") && !hasTriggered together come out as 'true'.  Next - need to disable controls from player and turn to an observer moment.

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics {
+	public class CinematicTrigger : MonoBehaviour{
+		bool hasTriggered = false;
+		void OnTriggerEnter(Collider other) {
+			if (GameObject.FindWithTag("Player") && !hasTriggered) {
+				GetComponent<PlayableDirector>().Play();
+				hasTriggered = true;
+			}
+		}
+	}
+}
