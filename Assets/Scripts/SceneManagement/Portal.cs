fe227424e26b58d1a364acdfc44d59c2b5a2477f
Robert Thiel <thielbert7@gmail.com>
2019-10-09 10:55:53 -0600

    Created a fence to signify portal to next level in original sandbox.  Created a Portal.cs script and added to a portal object, resized with a box collider and rigidbody to be allowed to register a triggered collision.  Portal.cs uses new namespace "SceneManagement".  Within Portal.cs, created an OnTriggerEnter event where only a "Player" tagged GameObject can trigger the event.  If it is the player, we get the active scene and create a variable for that and a next scene.  If the next scene is less than the total scene count available, we load the next scene.  Otherwise, we go back to the first scene.

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement {
+	public class Portal : MonoBehaviour {
+		// GameObject player;
+
+		// void Start() {
+		// 	player = GameObject.FindWithTag("Player");
+		// }
+
+		void OnTriggerEnter(Collider other) {
+			if (other.tag == "Player") {
+				int activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+				int nextSceneIndex = activeSceneIndex + 1;
+				if (nextSceneIndex < SceneManager.sceneCountInBuildSettings) {
+					SceneManager.LoadScene(nextSceneIndex);
+				} else {
+					SceneManager.LoadScene(0);
+				}
+			}
+		}
+	}
+}
