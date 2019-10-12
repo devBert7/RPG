5487bd3b4bd60a8e83b2cb37dae86701437f7db2
Robert Thiel <thielbert7@gmail.com>
2019-10-12 10:17:14 -0600

    Imported ISaveable.cs, SaveableEntity.cs, SavingSystem.cs, and SerializableVectore3.cs from lecture resources.  Created a SavingWrapper.cs file where I access the SavingSystem.cs component and attach the wrapper to a Saving prefab within the PersistentObjects object in Unity.  Within the wrapper, I watch for Key Down of S or L and respectively invoke Save() or Load() on a default save file which is created in my file path under "save.sav".

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement {
+	public class SavingWrapper : MonoBehaviour {
+		const string defaultSaveFile = "save";
+
+		SavingSystem saveSys;
+
+		void Start() {
+			saveSys = GetComponent<SavingSystem>();
+		}
+
+		void Update() {
+			if (Input.GetKeyDown(KeyCode.L)) {
+				Load();
+			}
+			
+			if (Input.GetKeyDown(KeyCode.S)) {
+				Save();
+			}
+		}
+
+		void Load() {
+			saveSys.Load(defaultSaveFile);
+		}
+
+		void Save() {
+			saveSys.Save(defaultSaveFile);
+		}
+	}
+}
