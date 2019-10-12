a25d8573bb035f4996def565f0e72c827901c4e2
Robert Thiel <thielbert7@gmail.com>
2019-10-12 11:36:07 -0600

    Set up Health.cs to take in the Saving namespace and use ISaveable.cs to set up CaptureState() and RestoreState() methods.  Capture takes in the character's health and then in Restore, we set the health by casting the state argument to a float.  We also check to see if the health of any saved character states is == 0 and if so, ensure they've died so we don't have zombies walking about.  (In past, only if they had 0 health && were attacked would they actually die) Lastly, set up a Die() method for that situation so we don't have to repeat our code.  DRY, baby!

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core {
-	public class Health : MonoBehaviour {
+	public class Health : MonoBehaviour, ISaveable {
 		[SerializeField] float health = 100f;
 
 		bool isDead = false;
@@ -16,9 +17,25 @@ namespace RPG.Core {
 			health = Mathf.Max(health - damage, 0);
 
 			if (health == 0 && !isDead) {
-				isDead = true;
-				GetComponent<Animator>().SetTrigger("Die");
-				GetComponent<ActionScheduler>().CancelCurrentAction();
+				Die();
+			}
+		}
+
+		void Die() {
+			isDead = true;
+			GetComponent<Animator>().SetTrigger("Die");
+			GetComponent<ActionScheduler>().CancelCurrentAction();
+		}
+
+		public object CaptureState() {
+			return health;
+		}
+
+		public void RestoreState(object state) {
+			health = (float)state;
+
+			if (health == 0 && !isDead) {
+				Die();
 			}
 		}
 	}
