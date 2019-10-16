5d19a35db24a24d6a75c84f5217c32c7f1f55b1d
Robert Thiel <thielbert7@gmail.com>
2019-10-16 10:36:16 -0600

    Created a projectiles prefab folder and placed within it an ArrowProjectile prefab (lecture resource).  Positioned the arrow along the correct axis (z).  Created Projectile.cs where I created a target (temporarily attached the player to in the inspector) and an arrowSpeed, both of which are exposed to the inspector.  In Update() I check to make sure the target isn't null and return if so.  From there, the projectile (arrow, in this case) transform.LookAt() ensures that the target has a capsuleCollider and if not, returns a simple target.position.  If the target does have a capsule collider, the LookAt() position becomes half the target's height, based on the capsule collider height.  Finally, we Translate the transform of the projectile to that target using the Vector3.forward (travels along the z axis) * the speed of the arrow we've selected at a framerate independent speed (Vector3.forward * arrowSpeed * Time.deltaTime).

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat {
+	public class Projectile : MonoBehaviour{
+		[SerializeField] Transform target = null;
+		[SerializeField] float arrowSpeed = 10f;
+
+		void Update() {
+			if (target == null) {
+				return;
+			}
+
+			transform.LookAt(GetAimLocation());
+			transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
+		}
+
+		Vector3 GetAimLocation() {
+			CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+
+			if (targetCapsule == null) {
+				return target.position;
+			}
+			
+			return target.position + Vector3.up * targetCapsule.height / 2;
+		}
+	}
+}
