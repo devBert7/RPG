577b2fc81c90f19c6ed73a4a898418a36d697815
Robert Thiel <thielbert7@gmail.com>
2019-10-16 15:51:38 -0600

    Projectile.cs: created damage variable and use it to pass into OnTriggerEnter > target.TakeDamage() which is set with SetTarget() when the target and weaponDamage are passed in from Weapon.cs > LaunchProjectile() > ProjectileInstance.SetTarget().  I also added a box collider with isTrigger = true set.  In all, this enables us to fire at our target and if the target is == to the collier object then it depleats health and destroys the arrow and otherwise returns from the statement.

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,7 +6,8 @@ using RPG.Core;
 namespace RPG.Combat {
 	public class Projectile : MonoBehaviour{
 		[SerializeField] float arrowSpeed = 10f;
-
+		
+		float damage = 0;
 		Health target = null;
 		
 		void Update() {
@@ -18,8 +19,9 @@ namespace RPG.Combat {
 			transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
 		}
 
-		public void SetTarget(Health target) {
+		public void SetTarget(Health target, float damage) {
 			this.target = target;
+			this.damage = damage;
 		}
 
 		Vector3 GetAimLocation() {
@@ -30,6 +32,16 @@ namespace RPG.Combat {
 			}
 			
 			return target.transform.position + Vector3.up * targetCapsule.height / 2;
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if (other.GetComponent<Health>() != target) {
+				return;
+			}
+
+			target.TakeDamage(damage);
+
+			Destroy(gameObject);
 		}
 	}
 }
