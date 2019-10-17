ea0d671851331a3f8abefd32e1205ef60664a9c4
Robert Thiel <thielbert7@gmail.com>
2019-10-17 11:55:37 -0600

    Adjusted Enemy and Player arrow projectile prefabs to be Red and Black arrow projectile prefabs where Red has isHoming = true and Black = false.  To support in Projectile.cs - created bool isHoming and instead of Update() > LookAt() always, only if target is not dead.  Otherwise, we get the position on each instantiation in Start() once.  if target.isDead = true, arrow flies past collider.  Similarly in Update() > if (isHoming) now checks for IsDead and only keeps aim if = false.  Fixes small arrow bugs where arrow destroys itself after death occurs or hangs around the collider of the enemy once dead.

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,16 +6,24 @@ using RPG.Core;
 namespace RPG.Combat {
 	public class Projectile : MonoBehaviour{
 		[SerializeField] float arrowSpeed = 10f;
+		[SerializeField] bool isHoming = true;
 		
 		float damage = 0;
 		Health target = null;
+
+		void Start() {
+			transform.LookAt(GetAimLocation());
+		}
 		
 		void Update() {
 			if (target == null) {
 				return;
 			}
 
-			transform.LookAt(GetAimLocation());
+			if (isHoming && !target.IsDead()) {
+				transform.LookAt(GetAimLocation());
+			}
+
 			transform.Translate(Vector3.forward * arrowSpeed * Time.deltaTime);
 		}
 
@@ -36,6 +44,10 @@ namespace RPG.Combat {
 
 		void OnTriggerEnter(Collider other) {
 			if (other.GetComponent<Health>() != target) {
+				return;
+			}
+
+			if (target.IsDead()) {
 				return;
 			}
 
